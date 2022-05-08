f395863e1c72c24e1d716a36d88a4c29b6d50c3d
fatahrez <fertahrez@gmail.com>
2022-05-08 12:46:12 +0300

    Add PulaAPI to send request to api using retrofit

--- data/src/main/java/com/fatahapps/data/remote/PulaApi.kt
+++ data/src/main/java/com/fatahapps/data/remote/PulaApi.kt
@@ -1,4 +1,14 @@
 package com.fatahapps.data.remote
 
+import com.fatahapps.data.remote.dto.survey.SurveyWrapperDTO
+import retrofit2.http.GET
+
 interface PulaApi {
+    companion object {
+        const val BASE_URL = "https://run.mocky.io/v3/"
+    }
+
+    @GET("d628facc-ec18-431d-a8fc-9c096e00709a")
+    suspend fun getQuestions(): SurveyWrapperDTO
+
 }
